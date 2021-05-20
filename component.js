e55207aa0de71e3b61f6da316fa752b1679247c9
Eduardo Castillo Fong <EDUARDCF90@GMAIL.COM>
2021-05-20 18:36:25 -0400

    Initial commit

---
+++ component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+    
+    function SayHello() {
+    return (
+        <div>
+            <h2>Hello world</h2>
+        </div>
+    );
+}
+
+ReactDOM.render(<SayHello/>, document.getElementById('container'));
+
