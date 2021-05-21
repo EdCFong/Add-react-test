accf19c74b4eecb54d26919ac225eff0f9edd742
Eduardo Castillo Fong <EDUARDCF90@GMAIL.COM>
2021-05-21 17:49:29 -0400

    Update May 21, 2021

--- component.js
+++ component.js
@@ -1,12 +1,11 @@
-import React from 'react';
-    
-    function SayHello() {
-    return (
-        <div>
-            <h2>Hello world</h2>
-        </div>
-    );
-}
 
-ReactDOM.render(<SayHello/>, document.getElementById('container'));
+    function SayHello() {
+        return (
+            <div>
+                <h2>Hello world</h2>
+            </div>
+        );
+    }
+    
+    ReactDOM.render(<SayHello/>, document.getElementById('container')); 
 
